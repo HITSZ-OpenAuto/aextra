740d36b4b5f84b37e6ac47e6e41dae1d1a14b0a1
kokoro2336 <2529677678@qq.com>
2025-09-29 17:23:06 +0800

    chore: fix config in uno.config.js.

--- uno.config.ts
+++ uno.config.ts
@@ -12,9 +12,9 @@ export default defineConfig({
       // Extend CSS with custom styles (e.g., modify headings, links, etc.)
       cssExtend: {
         "h1, h2, h3, h4, h5, h6": {
-          margin: "2px",
-          position: "relative",
-          width: "100%",
+          "margin": "2px",
+          "position": "relative",
+          "width": "100%",
           "padding-bottom": "10px",
           "border-bottom": "1px solid rgba(230, 230, 230, 0.911)",
           "--md-after-opacity": "0",
@@ -26,11 +26,11 @@ export default defineConfig({
         },
 
         ".md-anchor::after": {
-          content: '"#"',
+          "content": "'#'",
           "font-weight": "600",
-          margin: "10px",
-          opacity: "var(--md-after-opacity)",
-          transition: "opacity 200ms ease",
+          "margin": "10px",
+          "opacity": "var(--md-after-opacity)",
+          "transition": "opacity 200ms ease",
         },
 
         ".md-anchor:hover::after": {
@@ -41,33 +41,33 @@ export default defineConfig({
           "--md-after-opacity": "0.2",
         },
 
-        ul: {
+        "ul": {
           "list-style-type": "disc",
-          margin: "15px 10px",
+          "margin": "15px 10px",
         },
 
-        li: {
-          margin: "6px 0 6px 0",
+        "li": {
+          "margin": "6px 0 6px 0",
         },
 
         "li a": {
-          color: "rgb(54, 151, 231)",
+          "color": "rgb(54, 151, 231)",
           "text-decoration": "underline",
           "text-underline-offset": "3px",
         },
 
-        p: {
-          margin: "15px 5px 25px 5px",
+        "p": {
+          "margin": "15px 5px 25px 5px",
         },
 
         "p a": {
-          color: "rgb(54, 151, 231)",
+          "color": "rgb(54, 151, 231)",
           "text-decoration": "underline",
           "text-underline-offset": "3px",
         },
 
-        img: {
-          margin: "auto",
+        "img": {
+          "margin": "auto",
           "margin-top": "40px",
           "margin-bottom": "40px",
         },
