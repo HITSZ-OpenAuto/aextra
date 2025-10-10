1cf36e5e3625216cf457fde113fca9f6a075e7de
syhanjin <syhanjin666@gmail.com>
2025-10-10 12:59:41 +0800

    deps(ui): migrate from UnoCSS to Tailwind CSS v4
    Since UnoCSS's UI library ecosystem is not perfect, we migrated back to tailwind4 to get more UI library support.

--- uno.config.ts
+++
@@ -1,7 +0,0 @@
-import presetAttributify from "@unocss/preset-attributify";
-import type { Preset } from "unocss";
-import { defineConfig, presetTypography, presetWind4 } from "unocss";
-
-export default defineConfig({
-  presets: [presetAttributify() as Preset, presetWind4() as Preset, presetTypography() as Preset],
-});
